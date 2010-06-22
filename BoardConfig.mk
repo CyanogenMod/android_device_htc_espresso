7d095e128577a8ce380236b462825ea617e69818
Wes Garner <wesgarner@gmail.com>
2010-06-22 17:04:34 -0500

    Updated BoardConfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -2,15 +2,12 @@
 # Product-specific compile-time definitions.
 #
 
-# Todo
 TARGET_BOARD_PLATFORM := msm7k
 TARGET_CPU_ABI := armeabi
-TARGET_CPU_ABI2 := 
-TARGET_ARCH_VARIANT := 
-TARGET_GLOBAL_CFLAGS += 
-TARGET_GLOBAL_CPPFLAGS += 
-
-TARGET_NO_BOOTLOADER := false
+# TARGET_ARCH_VARIANT := 
+TARGET_NO_BOOTLOADER := true
+TARGET_BOOTLOADER_BOARD_NAME := espresso
+TARGET_OTA_ASSERT_DEVICE := espresso
 
 #HARDWARE_OMX := true
 
@@ -24,18 +21,14 @@ WIFI_DRIVER_MODULE_ARG      := ""
 WIFI_DRIVER_MODULE_NAME     := "tiwlan_drv"
 WIFI_FIRMWARE_LOADER        := "wlan_loader"
 
-TARGET_BOOTLOADER_BOARD_NAME := espresso
-
+BOARD_USES_GENERIC_AUDIO := false
+BOARD_USE_HTC_LIBSENSORS := true
+BOARD_HTC_LIBSENSORS_NAME := sensors.espresso.so
 BOARD_KERNEL_CMDLINE := "mem=75M console=ttyMSM2,115200n8"
-
 BOARD_HAVE_BLUETOOTH := true
-
 BOARD_GL_TEX_POW2_DIMENSION_REQUIRED := false
-
 TARGET_HARDWARE_3D := true
-
 BOARD_GPS_LIBRARIES := libgps
-
 USE_PV_WINDOWS_MEDIA := false
 
 # Image sizes todo
@@ -44,10 +37,10 @@ BOARD_RECOVERYIMAGE_MAX_SIZE :=
 BOARD_SYSTEMIMAGE_MAX_SIZE := 
 BOARD_USERDATAIMAGE_MAX_SIZE := 
 # The size of a block that can be marked bad.
-BOARD_FLASH_BLOCK_SIZE := 
+# BOARD_FLASH_BLOCK_SIZE := 
 
 # Check accuracy of kernel base
-BOARD_KERNEL_BASE := 0xffff0000
+BOARD_KERNEL_BASE := 0x00000000
 USE_CAMERA_STUB := false
 BOARD_EGL_CFG := vendor/htc/espresso-open/egl.cfg
 
