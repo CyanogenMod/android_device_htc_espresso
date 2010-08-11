7a8039770c425d252c8d957aaa825ed3fec3b09c
Steve Kondik <shade@chemlab.org>
2010-08-11 14:50:31 -0400

    espresso: Lights module should be marked optional

--- liblights/Android.mk
+++ liblights/Android.mk
@@ -12,19 +12,22 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
 LOCAL_PATH:= $(call my-dir)
-# HAL module implemenation, not prelinked and stored in
-# hw/<COPYPIX_HARDWARE_MODULE_ID>.<ro.board.platform>.so
+
+ifneq ($(TARGET_SIMULATOR),true)
+
 include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := lights.c
-
-LOCAL_PRELINK_MODULE := false
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
-
-LOCAL_SHARED_LIBRARIES := liblog
 
 LOCAL_MODULE := lights.espresso
 
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_SRC_FILES := lights.c
+LOCAL_SHARED_LIBRARIES := liblog
+LOCAL_PRELINK_MODULE := false
+
 include $(BUILD_SHARED_LIBRARY)
+
+endif # !TARGET_SIMULATOR
